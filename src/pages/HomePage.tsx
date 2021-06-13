2333913db717f5671f20e1cfd8c387c2dd5afb4d
Ameet Shrestha <shrsth.ameet@gmail.com>
2021-06-13 19:24:37 +0545

    first commit

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const HomePage: React.FC = () => {
+	return (
+		<div>
+			<p>Typescript tutorial.</p>
+		</div>
+	);
+};
---
+++ src/redux/reducers/repositoriesReducer.ts
@@ -0,0 +1,5 @@
+const initialState = {
+	loading: false,
+	data: [],
+	error: null,
+};
